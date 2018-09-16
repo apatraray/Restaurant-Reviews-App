6cd63c9de9e97be09de0bfd6be05f13cd04ab3c1
anita <anita.ani2007@gmail.com>
2018-09-15 18:51:32 -0700

    changes are done in css file for the better view

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -160,18 +160,22 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.id = 'reviewer-name';
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.id = 'review-date'
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.id = 'reviewer-rating'
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
   const comments = document.createElement('p');
+  comments.id = 'review-comments';
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
