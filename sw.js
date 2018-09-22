3ccc9abdcb5daccdc92d84bb6091afb9a33f845c
anita <anita.ani2007@gmail.com>
2018-09-21 19:34:17 -0700

    code for service worker implementation

---
+++ sw.js
@@ -0,0 +1,61 @@
+var newCacheName = 'restaurant-review';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(newCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html?id=1',
+        '/restaurant.html?id=2',
+        '/restaurant.html?id=3',
+        '/restaurant.html?id=4',
+        '/restaurant.html?id=5',
+        '/restaurant.html?id=6',
+        '/restaurant.html?id=7',
+        '/restaurant.html?id=8',
+        '/restaurant.html?id=9',
+        '/restaurant.html?id=10',
+        '/js/main.js',
+        '/js/dbhelper.js',
+        '/js/restaurant_info.js',
+        '/data/restaurants.json',
+        '/css/styles.css',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant') &&
+                 cacheName != newCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
